fc8cd8b02e0c1db1a83f1945c2117f6b26a77c3f
AnnieCunniff <anniecunniff@kubrickgroup.com>
2025-04-01 15:53:34 +0100

    added init file

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -3,3 +3,7 @@ from converter import convert
 def test_convert():
     assert convert(100, 0.5) == 50
     assert round(convert(123.45, 1.23), 4) == 151.8435
+
+#from fetcher import get_exchange_rate
+#def test_get_exchange_rate():
+    
