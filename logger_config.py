f2c1e54ab243747dae75ec9a925225112c019971
Parmeat <parmeat_20@hotmail.com>
2025-04-01 16:08:56 +0100

    Update logger_config.py to include stream handling and docstring and comments

--- logger_config.py
+++ logger_config.py
@@ -5,13 +5,28 @@ Capture program flow and errors using Python's built-in logging module
 import logging
 
 def setup_logging():
+
+    """
+    Sets up a logger with seperate file and stream handlers. 
+    It will log DEBUG and up to a seperate file and INFO and up to the console
+
+    RETURNS:
+        logging.logger: configures logger instance
+    """
     logger = logging.getLogger("currency_converter")
-    logger.setLevel(logging.INFO)
+    logger.setLevel(logging.DEBUG)
 
-    if not logger.handlers:
+    if not logger.handlers: #Prevents duplicates handlers
+
+        # File handler will log everything
         fh = logging.FileHandler("converter.log")
-        sh = logging.StreamHandler()
+        fh.setLevel(logging.DEBUG)
 
+        #stream handler will show info and up.
+        sh = logging.StreamHandler()
+        sh.setLevel(logging.INFO)
+        
+        #formatter
         formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
         fh.setFormatter(formatter)
         sh.setFormatter(formatter)
